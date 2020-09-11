6a46d0fbab47cf5cc21d98003bf2317d60906763
SummersFan <978649973@qq.com>
2020-09-11 11:06:03 +0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.sparse import csr_matrix
+from sklearn.neighbors import NearestNeighbors
+from sklearn.decomposition import TruncatedSVD
